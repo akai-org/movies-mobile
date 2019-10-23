025bcb62345f06e6c2adadae9c01308c58cf7f8c
wojtek <krkezc@gmail.com>
2019-10-23 14:26:25 +0200

    added downloading details

---
+++ app/src/main/java/pl/org/akai/movies/data/DetailsResponse.kt
@@ -0,0 +1,112 @@
+package pl.org.akai.movies.data
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+/*
+http://www.omdbapi.com/?i=tt3896198&apikey=dbeb1564
+["Title", "Year", "Rated", "Released", "Runtime", "Genre", "Director", "Writer", "Actors", "Plot", "Language", "Country", "Awards", "Poster", "Ratings", "Metascore", "imdbRating", "imdbVotes", "imdbID", "Type", "DVD", "BoxOffice", "Production", "Website", "Response"]
+ */
+
+data class DetailsResponse(
+    @Expose
+    @SerializedName("Title")
+    val title: String,
+
+    @Expose
+    @SerializedName("Year")
+    val year: String,
+
+    @Expose
+    @SerializedName("Rated")
+    val rated: String,
+
+    @Expose
+    @SerializedName("Released")
+    val released: String,
+
+    @Expose
+    @SerializedName("Runtime")
+    val runtime: String,
+
+    @Expose
+    @SerializedName("Genre")
+    val genre: String,
+
+    @Expose
+    @SerializedName("Director")
+    val director: String,
+
+    @Expose
+    @SerializedName("Writer")
+    val writer: String,
+
+    @Expose
+    @SerializedName("Actors")
+    val actors: String,
+
+    @Expose
+    @SerializedName("Plot")
+    val plot: String,
+
+    @Expose
+    @SerializedName("Language")
+    val language: String,
+
+    @Expose
+    @SerializedName("Country")
+    val country: String,
+
+    @Expose
+    @SerializedName("Awards")
+    val awards: String,
+
+    @Expose
+    @SerializedName("Poster")
+    val poster: String,
+
+    @Expose
+    @SerializedName("Ratings ")
+    val ratings: List<Rating>,
+
+    @Expose
+    @SerializedName("Metascore")
+    val metascore: String,
+
+    @Expose
+    @SerializedName("imdbRating")
+    val imdbRating: String,
+
+    @Expose
+    @SerializedName("imdbVotes")
+    val imdbVotes: String,
+
+    @Expose
+    @SerializedName("imdbID")
+    val imdbID: String,
+
+    @Expose
+    @SerializedName("Type")
+    val type: String,
+
+    @Expose
+    @SerializedName("DVD")
+    val dvd: String,
+
+    @Expose
+    @SerializedName("BoxOffice")
+    val boxOffice: String,
+
+    @Expose
+    @SerializedName("Production")
+    val production: String,
+
+    @Expose
+    @SerializedName("Website")
+    val website: String,
+
+    @Expose
+    @SerializedName("Response")
+    val response: String
+
+)
--- app/src/main/java/pl/org/akai/movies/services/OMDbService.kt
+++ app/src/main/java/pl/org/akai/movies/services/OMDbService.kt
@@ -1,5 +1,6 @@
 package pl.org.akai.movies.services
 
+import pl.org.akai.movies.data.DetailsResponse
 import pl.org.akai.movies.data.SearchRespone
 import retrofit2.Call
 import retrofit2.http.GET
@@ -7,8 +8,11 @@ import retrofit2.http.Query
 
 interface OMDbService {
 
-
     @GET("/")
     fun getSerchedMovies(@Query("apikey") apikey: String, @Query("s") query: String):
             Call<SearchRespone>
+
+    @GET("/")
+    fun getMovieDetails(@Query("apikey") apikey: String, @Query("i") query: String):
+            Call<DetailsResponse>
 }
