c35abe47579f05e4fa1d6bd1a20908f6ad0ffca4
Kossa123 <33512619+Kossa123@users.noreply.github.com>
2019-11-04 22:14:00 +0100

    pr fixes, new version 1.0.4

--- app/src/main/java/pl/org/akai/movies/fragments/MovieDetailsFragment.kt
+++ app/src/main/java/pl/org/akai/movies/fragments/MovieDetailsFragment.kt
@@ -1,14 +1,10 @@
 package pl.org.akai.movies.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_movie_details.*
-import kotlinx.android.synthetic.main.fragment_movie_details.view.*
-import kotlinx.android.synthetic.main.movie_list_item.view.*
 import pl.org.akai.movies.R
 import pl.org.akai.movies.data.DetailsResponse
 import retrofit2.Call
@@ -21,8 +17,6 @@ class MovieDetailsFragment : BaseFragment() {
         get() = R.layout.fragment_movie_details
 
 
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         backButton.setOnClickListener {
@@ -32,45 +26,38 @@ class MovieDetailsFragment : BaseFragment() {
             .enqueue(object : Callback<DetailsResponse> {
                 override fun onFailure(call: Call<DetailsResponse>, t: Throwable) {}
 
-                val appContext = context!!.applicationContext
 
                 override fun onResponse(
                     call: Call<DetailsResponse>, response: Response<DetailsResponse>
                 ) {
                     when (response.code()) {
-                        200 ->
-                        {
-                            title.text = response.body()!!.title
-                            year.text = "${year.text}${response.body()!!.year}"
-                            rated.text = "${rated.text}${response.body()!!.rated}"
-                            released.text = "${released.text}${response.body()!!.released}"
-                            genre.text = "${genre.text}${response.body()!!.genre}"
-                            director.text = "${director.text}${response.body()!!.director}"
-                            runtime.text = "${runtime.text}${response.body()!!.runtime}"
-                            writer.text = "${writer.text}${response.body()!!.writer}"
-                            actors.text = "${actors.text}${response.body()!!.actors}"
-                            metascore.text = "${metascore.text}${response.body()!!.metascore}"
-                            dvd.text = "${dvd.text}${response.body()!!.dvd}"
-                            boxOffice.text = "${boxOffice.text}${response.body()!!.boxOffice}"
-                            production.text = "${production.text}${response.body()!!.production}"
-                            website.text = "${website.text}${response.body()!!.website}"
-
-                            plot.text = response.body()!!.plot
-
-                            Glide.with(appContext).load(response.body()!!.poster).into(poster)
-
+                        200 -> {
+                            setupMovieData(response.body()!!)
 
                         }
-
-
                     }
-
-
-
-
-
                 }
-
             })
+    }
+
+    fun setupMovieData(detailsResponse: DetailsResponse) {
+        title.text = detailsResponse.title
+        year.text = getString(R.string.year, detailsResponse.year)
+        rated.text = getString(R.string.rated, detailsResponse.rated)
+        released.text = getString(R.string.released, detailsResponse.released)
+        genre.text = getString(R.string.genre, detailsResponse.genre)
+        director.text = getString(R.string.director, detailsResponse.director)
+        runtime.text = getString(R.string.runtime, detailsResponse.runtime)
+        writer.text = getString(R.string.writer, detailsResponse.writer)
+        actors.text = getString(R.string.actors, detailsResponse.actors)
+        metascore.text = getString(R.string.metascore, detailsResponse.metascore)
+        dvd.text = getString(R.string.dvd, detailsResponse.dvd)
+        boxOffice.text = getString(R.string.box_office, detailsResponse.boxOffice)
+        production.text = getString(R.string.production, detailsResponse.production)
+        website.text = getString(R.string.website, detailsResponse.website)
+
+        plot.text = detailsResponse.plot
+
+        Glide.with(context!!).load(detailsResponse.poster).into(poster)
     }
 }
