56b6738317556d6432e1c208df9fd6391ba88389
V-Ader <karol.zawislak@gmail.com>
2019-11-04 17:41:28 +0100

    connecting data with layout

--- app/src/main/java/pl/org/akai/movies/fragments/MovieDetailsFragment.kt
+++ app/src/main/java/pl/org/akai/movies/fragments/MovieDetailsFragment.kt
@@ -3,8 +3,12 @@ package pl.org.akai.movies.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_movie_details.*
+import kotlinx.android.synthetic.main.fragment_movie_details.view.*
+import kotlinx.android.synthetic.main.movie_list_item.view.*
 import pl.org.akai.movies.R
 import pl.org.akai.movies.data.DetailsResponse
 import retrofit2.Call
@@ -17,6 +21,8 @@ class MovieDetailsFragment : BaseFragment() {
         get() = R.layout.fragment_movie_details
 
 
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         backButton.setOnClickListener {
@@ -26,15 +32,45 @@ class MovieDetailsFragment : BaseFragment() {
             .enqueue(object : Callback<DetailsResponse> {
                 override fun onFailure(call: Call<DetailsResponse>, t: Throwable) {}
 
+                val appContext = context!!.applicationContext
+
                 override fun onResponse(
                     call: Call<DetailsResponse>, response: Response<DetailsResponse>
                 ) {
                     when (response.code()) {
-                        200 -> Log.d("MyLogMDF", "${response.body()}")
+                        200 ->
+                        {
+                            title.text = response.body()!!.title
+                            year.text = "${year.text}${response.body()!!.year}"
+                            rated.text = "${rated.text}${response.body()!!.rated}"
+                            released.text = "${released.text}${response.body()!!.released}"
+                            genre.text = "${genre.text}${response.body()!!.genre}"
+                            director.text = "${director.text}${response.body()!!.director}"
+                            runtime.text = "${runtime.text}${response.body()!!.runtime}"
+                            writer.text = "${writer.text}${response.body()!!.writer}"
+                            actors.text = "${actors.text}${response.body()!!.actors}"
+                            metascore.text = "${metascore.text}${response.body()!!.metascore}"
+                            dvd.text = "${dvd.text}${response.body()!!.dvd}"
+                            boxOffice.text = "${boxOffice.text}${response.body()!!.boxOffice}"
+                            production.text = "${production.text}${response.body()!!.production}"
+                            website.text = "${website.text}${response.body()!!.website}"
+
+                            plot.text = response.body()!!.plot
+
+                            Glide.with(appContext).load(response.body()!!.poster).into(poster)
+
+
+                        }
+
+
                     }
+
+
+
+
+
                 }
 
             })
     }
 }
-
