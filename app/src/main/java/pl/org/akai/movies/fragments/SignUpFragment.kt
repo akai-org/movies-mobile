5ccadff5c8a2bddf09dd5726396e9eb284a996f9
wojtek <krkezc@gmail.com>
2019-11-07 12:34:12 +0100

    made sign up fragment layout

---
+++ app/src/main/java/pl/org/akai/movies/fragments/SignUpFragment.kt
@@ -0,0 +1,23 @@
+package pl.org.akai.movies.fragments
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import pl.org.akai.movies.R
+
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+
+}
