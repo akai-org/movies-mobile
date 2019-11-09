c1768bc2ceb426115693c1691764c2105f675b34
wojtek <krkezc@gmail.com>
2019-11-09 23:52:31 +0100

    removed inheritance from BaseFragment and a small clean up

--- app/src/main/java/pl/org/akai/movies/fragments/SignUpFragment.kt
+++ app/src/main/java/pl/org/akai/movies/fragments/SignUpFragment.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
@@ -14,26 +13,17 @@ import kotlinx.android.synthetic.main.fragment_sign_up.*
 import pl.org.akai.movies.R
 
 
-class SignUpFragment : BaseFragment() {
+class SignUpFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
 
-    override val layoutId: Int
-        get() = R.layout.fragment_sign_up
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        auth = FirebaseAuth.getInstance()
-//        signUpButton.setOnClickListener {
-//            val email: String = emailEditText.text.toString()
-//            val password: String = passwordEditText.text.toString()
-//            createUserWithEmailAndPassword(email, password)
-//        }
-//        return super.onCreateView(inflater, container, savedInstanceState)
-//    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         auth = FirebaseAuth.getInstance()
@@ -44,9 +34,7 @@ class SignUpFragment : BaseFragment() {
         }
     }
 
-    // d250393@urhen.com
-    // 123456
-    fun createUserWithEmailAndPassword(email: String, password: String) {
+    private fun createUserWithEmailAndPassword(email: String, password: String) {
         if (email.isEmpty() || password.isEmpty()) {
             showErrorMessage(getString(R.string.wrongEmailOrPassword))
             return
@@ -55,8 +43,9 @@ class SignUpFragment : BaseFragment() {
             .addOnCompleteListener(activity!!) { task ->
                 if (task.isSuccessful) {
                     Log.d("Firebase", "createUserWithEmail:success")
-                    val user = auth.currentUser
                     errorTextView.text = ""
+                    Toast.makeText(context, getString(R.string.accountCreated), Toast.LENGTH_LONG)
+                        .show()
 
                 } else {
                     Log.d("Firebase", "createUserWithEmail: error: ${task.exception}")
